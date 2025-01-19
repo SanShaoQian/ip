af53bc010ed2b5ff592b822b507b250ddd704af3
SanShaoQian <sanshaoqian@gmail.com>
2025-01-19 17:44:56 +0800

    Added Mark as Done
    Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Stonks.java
+++ src/main/java/Stonks.java
@@ -12,19 +12,37 @@ public class Stonks {
 
         Scanner scanner = new Scanner(System.in);
         String input;
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
         while (true) {
             input = scanner.nextLine();
+            String[] in = input.split(" ");
             if (input.equals("bye")) break;
-            if (input.equals("list")) {
+            else if (input.equals("list")) {
                 System.out.print(separator);
+                System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println(String.format("     %d. %s", i + 1, list.get(i)));
+                    System.out.println(String.format("     %d. %s", i + 1, list.get(i).toString()));
                 }
                 System.out.print(separator);
             }
+            else if (in[0].equals("mark")) {
+                int index = Integer.parseInt(in[1]) - 1;
+                System.out.print(separator);
+                System.out.println("     Nice! I've marked this task as done:");
+                list.get(index).markDone();
+                System.out.println("       " + list.get(index).toString());
+                System.out.print(separator);
+            }
+            else if (in[0].equals("unmark")) {
+                int index = Integer.parseInt(in[1]) - 1;
+                System.out.print(separator);
+                System.out.println("     OK, I've marked this task as not done yet:");
+                list.get(index).markNotDone();
+                System.out.println("       " + list.get(index).toString());
+                System.out.print(separator);
+            }
             else {
-                list.add(input);
+                list.add(new Task(input));
                 System.out.print(String.format("%s     added: %s\n%s", separator, input, separator));
             }
         }
