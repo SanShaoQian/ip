dabd404ff4e31ba54a32ee13fc4361f9573535df
SanShaoQian <sanshaoqian@gmail.com>
2025-01-21 14:09:41 +0800

    Add ToDos, Events, Deadlines
    Add support for tracking three types of tasks:
    
    ToDos: tasks without any date/time attached to it e.g., visit new theme park
    Deadlines: tasks that need to be done before a specific date/time e.g., submit report by 11/10/2019 5pm
    Events: tasks that start at a specific date/time and ends at a specific date/time
    e.g., (a) team project meeting 2/10/2019 2-4pm (b) orientation week 4/10/2019 to 11/10/2019

--- src/main/java/Stonks.java
+++ src/main/java/Stonks.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Stonks {
     public static void main(String[] args) {
@@ -12,12 +14,13 @@ public class Stonks {
 
         Scanner scanner = new Scanner(System.in);
         String input;
+        String[] inputs;
         List<Task> list = new ArrayList<>();
         while (true) {
             input = scanner.nextLine();
-            String[] in = input.split(" ");
-            if (input.equals("bye")) break;
-            else if (input.equals("list")) {
+            inputs = input.split(" ");
+            if (inputs[0].equals("bye")) break;
+            else if (inputs[0].equals("list")) {
                 System.out.print(separator);
                 System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
@@ -25,25 +28,48 @@ public class Stonks {
                 }
                 System.out.print(separator);
             }
-            else if (in[0].equals("mark")) {
-                int index = Integer.parseInt(in[1]) - 1;
+            else if (inputs[0].equals("mark")) {
+                int index = Integer.parseInt(inputs[1]) - 1;
                 System.out.print(separator);
                 System.out.println("     Nice! I've marked this task as done:");
                 list.get(index).markDone();
                 System.out.println("       " + list.get(index).toString());
                 System.out.print(separator);
             }
-            else if (in[0].equals("unmark")) {
-                int index = Integer.parseInt(in[1]) - 1;
+            else if (inputs[0].equals("unmark")) {
+                int index = Integer.parseInt(inputs[1]) - 1;
                 System.out.print(separator);
                 System.out.println("     OK, I've marked this task as not done yet:");
                 list.get(index).markNotDone();
                 System.out.println("       " + list.get(index).toString());
                 System.out.print(separator);
             }
-            else {
-                list.add(new Task(input));
-                System.out.print(String.format("%s     added: %s\n%s", separator, input, separator));
+            else if (inputs[0].equals("todo") || inputs[0].equals("deadline") || inputs[0].equals("event")) {
+                System.out.print(separator);
+                System.out.println("     Got it. I've added this task:");
+                Task task = new Task("");
+                if (inputs[0].equals("todo")) {
+                    String regex = "^\\s*(\\w+)\\s+(.*?)\\s*$";
+                    Pattern pattern = Pattern.compile(regex);
+                    Matcher matcher = pattern.matcher(input);
+                    if (matcher.matches()) task = new Todo(matcher.group(2));
+                }
+                else if (inputs[0].equals("deadline")) {
+                    String regex = "^\\s*(\\w+)\\s+(.*?)\\s+/by\\s+(.+)\\s*$";
+                    Pattern pattern = Pattern.compile(regex);
+                    Matcher matcher = pattern.matcher(input);
+                    if (matcher.matches()) task = new Deadline(matcher.group(2), matcher.group(3));
+                }
+                else if (inputs[0].equals("event")) {
+                    String regex = "^\\s*(\\w+)\\s+(.*?)\\s+/from\\s+(.+?)\\s+/to\\s+(.+)\\s*$";
+                    Pattern pattern = Pattern.compile(regex);
+                    Matcher matcher = pattern.matcher(input);
+                    if (matcher.matches()) task = new Event(matcher.group(2), matcher.group(3), matcher.group(4));
+                }
+                list.add(task);
+                System.out.println("       " + task);
+                System.out.println(String.format("     Now you have %d tasks in the list.", list.size()));
+                System.out.print(separator);
             }
         }
         System.out.println(separator + exit + separator);
